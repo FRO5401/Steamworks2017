955f212784f20d4adda2e3efccff7e6413e72bf1
Matt Jordan <atomicbrawlers@gmail.com>
2017-01-11 21:04:42 -0500

    Work In Progress
    Small amounts of vision/shooter psuedocode

--- src/org/usfirst/frc/team5401/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/Shooter.java
@@ -6,10 +6,16 @@ import edu.wpi.first.wpilibj.command.PIDSubsystem;
  *
  */
 public class Shooter extends PIDSubsystem {
+	
+	//declare shooter motors
+	//declare pid stuff?
 
     // Initialize your subsystem here
     public Shooter() {
-    	super("shooter",1,2,3); //XXX Temporary to get rid of error status
+    	super("shooter",1,2,3); //initializes pid //XXX Temporary to get rid of error status
+    	
+    	//initialize shooter motors
+    	
         // Use these to get going:
         // setSetpoint() -  Sets where the PID controller should move the system
         //                  to
@@ -25,11 +31,15 @@ public class Shooter extends PIDSubsystem {
         // Return your input value for the PID loop
         // e.g. a sensor, like a potentiometer:
         // yourPot.getAverageVoltage() / kYourMaxVoltage;
+    	
+    	//return PE sensor 
         return 0.0;
     }
 
     protected void usePIDOutput(double output) {
         // Use output to drive your system, like a motor
         // e.g. yourMotor.set(output);
+    	
+    	//set motor 
     }
 }
