7784569b366de4bee20cc4be9059a20b8539d548
jxfireruby <jxfireruby@gmail.com>
2017-01-21 10:13:55 -0500

    Finished merging Shooter branch

--- src/org/usfirst/frc/team5401/robot/commands/FlyWheelControl.java
+++ src/org/usfirst/frc/team5401/robot/commands/FlyWheelControl.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc.team5401.robot.commands;
 
-import org.usfirst.frc.team5401.robot.Robots;
+import org.usfirst.frc.team5401.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -12,14 +12,18 @@ public class FlyWheelControl extends Command {
     public FlyWheelControl() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.shooter);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.shooter.reset();
+    	Robot.shooter.startMotors();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -29,10 +33,13 @@ public class FlyWheelControl extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.shooter.reset();
     }
 
+    
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.shooter.reset();
     }
 }
