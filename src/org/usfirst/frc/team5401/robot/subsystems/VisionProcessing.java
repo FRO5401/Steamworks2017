2ca92c7a7f24c2699bf1fae538e174ef21a2f660
Kevin McKay <kmckay@bensalemsd.org>
2017-01-25 20:30:59 -0500

    Coded

--- src/org/usfirst/frc/team5401/robot/subsystems/VisionProcessing.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/VisionProcessing.java
@@ -1,6 +1,14 @@
 package org.usfirst.frc.team5401.robot.subsystems;
-
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import org.usfirst.frc.team5401.robot.TargetMap;
+
+
+import com.jcraft.jsch.JSchException;
 
 /**
  *
@@ -9,11 +17,34 @@ public class VisionProcessing extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
-	//declare rpi's
+	String host="pulsatrix.local";
+    String user="pi";
+    String password="raspberry";
+    String startCommand="java -Djava.library.path=/usr/local/share/OpenCV/java -jar Webcam.java &";
+    String terminateCommand="killall java";
+    Channel channel;
+	Session session;
+	static NetworkTable targetRect;//XXX
+	double networkDefault = -99;
+	private double target[];
 	
 	public VisionProcessing(){
-		//initialize rpi's
+		java.util.Properties config = new java.util.Properties(); 
+    	config.put("StrictHostKeyChecking", "no");
+    	JSch jsch = new JSch();
+		try {
+			session = jsch.getSession(user, host, 22);
+	    	session.setPassword(password);
+	    	session.setConfig(config);
+	    	session.connect();
+			channel=session.openChannel("exec");
+		} catch (JSchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try{Thread.sleep(1000);}catch(Exception ee){}
+
+    	System.out.println("Connected");
 	}
 	
     public void initDefaultCommand() {
@@ -21,10 +52,31 @@ public class VisionProcessing extends Subsystem {
         //setDefaultCommand(new MySpecialCommand());
     }
     
-    //get shooter pi data method
-    	//send correct positioning
+    public boolean beginTargeting() {
+    	((ChannelExec)channel).setCommand(startCommand);
+        channel.setInputStream(null);
+    	return true;
+    }
+
+    public boolean terminateTargeting() {
+    	((ChannelExec)channel).setCommand(terminateCommand);
+        channel.setInputStream(null);
+        channel.disconnect();
+        session.disconnect();
+        System.out.println("DONE");
+    	return true;
+    }
     
-    //get spike pi data method
-    	//send correct positioning
+    public boolean readTargetingData(){
+    	targetRect = NetworkTable.getTable("BoilerPipeLineOut");
+    	target[0] =  targetRect.getNumber("X", networkDefault);
+    	target[1] =  targetRect.getNumber("Y", networkDefault);
+    	target[2] =  targetRect.getNumber("height", networkDefault);
+    	target[3] =  targetRect.getNumber("width", networkDefault);//XXX
+    	System.out.println(target);
+
+    	return true;
+    }
+
 }
 
