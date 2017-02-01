7f9817ba489fa60277b9c7108cf57895fc2954d4
AtomicBrawlers <atomicbrawlers@gmail.com>
2017-02-01 18:53:06 -0500

    Started JavaDoc on Loader

--- src/org/usfirst/frc/team5401/robot/subsystems/Loader.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/Loader.java
@@ -19,6 +19,17 @@ public class Loader extends Subsystem {
 		
 	private double LOADER_SPEED;
 	
+	/** 
+	 *  <p>
+	 *  This subsystem is used to load balls into the shooter. It runs
+	 *  all the time starting in autonomous.
+	 *  </p>
+	 *  
+	 *  <p>
+	 *  Instantiates the left and right conveyer. Sets the loader speed.
+	 *  Puts conveyer status on the SmartDashboard.
+	 *  </p>
+	 */
 	public Loader(){
 		//initialize Victor motor
 		leftConveyor  = new VictorSP(RobotMap.LOADER_CONVEYOR_LEFT);
@@ -33,12 +44,24 @@ public class Loader extends Subsystem {
     }
     
     //load action method (direction parameter)
-    	//send motor values 
+    	//send motor values
+    
+    /** Runs the conveyers
+     *  
+     *  <p> 
+     *  NOTE: Fix spelling in code
+     *  </p><p>
+     *  Sets the conveyers to run at the default speed
+     *  </p>
+     */
     public void runConveyors(){
     	leftConveyor .set(LOADER_SPEED);
     	rightConveyor.set(LOADER_SPEED);
     }
     
+    /** Turns off the conveyers
+     * 
+     */
     public void stopConveyors(){
     	leftConveyor .set(0);
     	rightConveyor.set(0);
