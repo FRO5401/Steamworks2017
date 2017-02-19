ef677081789293e1bb71702cf869df8586cf4ef1
Jxfireruby <jxfireruby@gmail.com>
2017-02-19 14:44:21 -0500

    Some progress, not much

--- src/org/usfirst/frc/team5401/robot/subsystems/DummyCameras.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/DummyCameras.java
@@ -30,18 +30,20 @@ public class DummyCameras extends Subsystem {
 		localHeight = RobotMap.CAMERA_HEIGHT;
 		localFPS = RobotMap.CAMERA_FPS;
     	
-		cam.getInstance().startAutomaticCapture(0);//0 is USB slot nearest to the center of the roboRIO.
+		//cam.getInstance().startAutomaticCapture(0);//0 is USB slot nearest to the center of the roboRIO.
 		
 		/******Under this is for Camera Switching**********/
 		
 		//Before, setSize and setFPS were needed. setSize was replaced by setResolution. However, setResolutiona and setFPS are in a different class other than CameraServer
     	//cam1 and cam0 are names given to camera by roboRIO. frontCamera and backCamera are names we give them
-		//frontCamera = cameraServer.getInstance().startAutomaticCapture("cam0", 0);//parameters are String, int. Many overloads with different parameters
-		//frontCamera.setResolution(localWidth, localHeight);
-		//frontCamera.setFPS(localFPS);
+		frontCamera = cameraServer.getInstance().startAutomaticCapture("cam0", 0);//parameters are String, int. Many overloads with different parameters
+		frontCamera.setResolution(localWidth, localHeight);
+		frontCamera.setFPS(localFPS);
 
-//		frontCamera.setResolution(localWidth, localHeight);
-//		frontCamera = setFPS(localFPS);
+		backCamera = cameraServer.getInstance().startAutomaticCapture("cam1", 1);//parameters are String, int. Many overloads with different parameters
+		backCamera.setResolution(localWidth, localHeight);
+		backCamera.setFPS(localFPS);
+
 //		backCamera = cameraServer.startAutomaticCapture("cam1", RobotMap.CAMERA_BACK);
 	}
 
