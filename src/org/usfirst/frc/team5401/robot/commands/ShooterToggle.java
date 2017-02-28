3d49218de25224f3140f8bef705af155a41c7826
ptoil <suhailmallick@gmail.com>
2017-02-27 19:04:32 -0500

    Shooter override for Voltage
    used to be PercentVBus

--- src/org/usfirst/frc/team5401/robot/commands/ShooterToggle.java
+++ src/org/usfirst/frc/team5401/robot/commands/ShooterToggle.java
@@ -32,6 +32,7 @@ public class ShooterToggle extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.shooter.stop();
     }
 
     // Called when another command which requires one or more of the same
