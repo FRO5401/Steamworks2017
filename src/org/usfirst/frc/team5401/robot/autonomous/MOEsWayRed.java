13daf8d0e49a98d81b15aeeffe495fadd3c7a5f0
ptoil <suhailmallick@gmail.com>
2017-04-06 13:52:31 -0400

    Auto Changes according to Practice Field Tests

--- src/org/usfirst/frc/team5401/robot/autonomous/MOEsWayRed.java
+++ src/org/usfirst/frc/team5401/robot/autonomous/MOEsWayRed.java
@@ -18,12 +18,12 @@ public class MOEsWayRed extends CommandGroup {
     	addSequential(new ShooterStop());
     	addSequential(new LoadShooter());
     	addSequential(new AutoTurnAngle(32, true, false));
-    	addSequential(new AutoDrive(-87, .9));
+    	addSequential(new AutoDrive(-47, .9));
     	addSequential(new AutoTurnAngle(-14, true, false));
-    	addSequential(new AutoDrive(-31,0.9));
+    	addSequential(new AutoDrive(-24,0.9));
     	addSequential(new AutoPopGearOnly(-1)); //out
 		addSequential(new WaitCommand(1));
-		addSequential(new AutoDrive(36, .9));
+		addSequential(new AutoDrive(24, .9));
 		addSequential(new AutoPopGearOnly(1)); //in
     	addSequential(new AutoTurnAngle(-40, true, false));//turn 45 //XXX need to change angle
     	
