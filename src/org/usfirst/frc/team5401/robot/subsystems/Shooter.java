ee9531bea0f48ecbebc2c8f6d21c3a0e6caff0e0
ptoil <suhailmallick@gmail.com>
2017-04-26 20:57:19 -0400

    Auto Tuning with MoeWay BLue

--- src/org/usfirst/frc/team5401/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/Shooter.java
@@ -18,7 +18,7 @@ public class Shooter extends Subsystem {
 	
 	
 	private double VOLTAGE_MOTOR_SPEED = -7.8;
-	private double PID_MOTOR_SPEED = -19900; //-19875 //-23750 original
+	private double PID_MOTOR_SPEED = -20000; //-19875 //-23750 original
 	private double MOTOR_SPEED = PID_MOTOR_SPEED;
 	private double feed_forward;
 	
@@ -67,7 +67,7 @@ public class Shooter extends Subsystem {
 	   
 	   kP = .1;
 	   kI = .000000005;
-	   kD = 4;
+	   kD = 8;//was 4
 	   //MOTOR_SPEED = SmartDashboard.getNumber("velocity", MOTOR_SPEED);
 	   
 	   Izone = 0;
