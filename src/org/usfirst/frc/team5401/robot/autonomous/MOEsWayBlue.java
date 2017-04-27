08f078e7a0059718c19e51523601bba8335c9681
jxfireruby <jxfireruby@gmail.com>
2017-04-27 18:25:04 -0400

    Blue MOES way edits

--- src/org/usfirst/frc/team5401/robot/autonomous/MOEsWayBlue.java
+++ src/org/usfirst/frc/team5401/robot/autonomous/MOEsWayBlue.java
@@ -24,12 +24,12 @@ public class MOEsWayBlue extends CommandGroup {
     	addSequential(new LoadShooter());
     	addSequential(new AutoTurnAngle(-28, true, false));
     	addSequential(new AutoDrive(-81, .9));
-    	addSequential(new AutoTurnAngle(30, true, false));
+    	addSequential(new AutoTurnAngle(15, true, false));
     	addSequential(new AutoDrive(-41,0.9));
     	addSequential(new WaitCommand(.75)); //wait before dropping gear
     	addSequential(new AutoPopGearOnly(-1)); //out
 		addSequential(new WaitCommand(1));
-		addSequential(new AutoDrive(30, .9));
+		addSequential(new AutoDrive(18, .9));
 		addSequential(new AutoPopGearOnly(1)); //in
     	addSequential(new AutoTurnAngle(32, true, false));//turn 45 //XXX need to change angle
     	
