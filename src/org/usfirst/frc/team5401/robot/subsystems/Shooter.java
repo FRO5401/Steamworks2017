80d9b7341e1162a4313c158d050488942973ba32
jxfireruby <jxfireruby@gmail.com>
2018-01-12 17:41:02 -0500

    Fixes complete, testing needed

--- src/org/usfirst/frc/team5401/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/Shooter.java
@@ -3,8 +3,11 @@ package org.usfirst.frc.team5401.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import com.ctre.CANTalon;
-import com.ctre.CANTalon.TalonControlMode;
+//import com.ctre.CANTalon;
+//import com.ctre.CANTalon.TalonControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
 /** Uses PID to lineup and shoot, also uses motors to shoot
 *
@@ -13,8 +16,8 @@ import com.ctre.CANTalon.TalonControlMode;
 public class Shooter extends Subsystem {
 	
 	//declare talon speed controller
-	CANTalon _talonMaster;
-	CANTalon _talonSlave;	
+	TalonSRX _talonMaster;
+	TalonSRX _talonSlave;	
 	
 	
 	private double VOLTAGE_MOTOR_SPEED = -7.8;
@@ -41,24 +44,35 @@ public class Shooter extends Subsystem {
 	    */
 	public Shooter() {
 	   	
-	   	_talonMaster = new CANTalon(0);
-	   	_talonSlave = new CANTalon(1);
-	    	
+	   	_talonMaster = new TalonSRX(0);
+	   	_talonSlave = new TalonSRX(1);
+	    
+	   	/*
     	_talonMaster.changeControlMode(TalonControlMode.Speed); //XXX Testing with this
 //	   	_talonMaster.setProfile(0);//XXX  Probably don't need this, but if shooter doesn't work try putting this back in first
 	   	_talonSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
     	_talonSlave.set(_talonMaster.getDeviceID());
+    	*/
+	   	_talonMaster.set(ControlMode.Velocity, 0);
+	   	_talonSlave.set(ControlMode.Follower, _talonMaster.getDeviceID());
 	   	
 	   	SmartDashboard.putNumber("motor_speed", MOTOR_SPEED);
 	   	SmartDashboard.putBoolean("AutoTargeting", false);
-	    	
+	    
+	   	/*
 	 	_talonMaster.getEncPosition();
 	   	SmartDashboard.putNumber("Position", _talonMaster.getEncPosition());
 	   	_talonMaster.getEncVelocity();
 	   	SmartDashboard.putNumber("Velocity",  _talonMaster.getEncVelocity());
 	   	SmartDashboard.putNumber("Velocity Raw",  _talonMaster.getEncVelocity()); //not to be used for graph
 	   	_talonMaster.setEncPosition(0);
-	   	
+	   	*/
+	   	_talonMaster.getSensorCollection().getQuadraturePosition();
+	   	SmartDashboard.putNumber("Position", _talonMaster.getSensorCollection().getQuadraturePosition());
+	   	_talonMaster.getSensorCollection().getQuadratureVelocity();
+	   	SmartDashboard.putNumber("Velocity",  _talonMaster.getSensorCollection().getQuadratureVelocity());
+	   	SmartDashboard.putNumber("Velocity Raw",  _talonMaster.getSensorCollection().getQuadratureVelocity()); //not to be used for graph
+	   	_talonMaster.getSensorCollection().setQuadraturePosition(0, 10); //second parameter is timeout value in ms. 10 is default value
 	   	
 	   feed_forward = .033;
 	   //kP = SmartDashboard.getNumber("kP", kP);
@@ -84,8 +98,8 @@ public class Shooter extends Subsystem {
 	   SmartDashboard.putNumber("kD", kD);
 	   SmartDashboard.putNumber("rampRate", rampRate);
 	    	
-	   	_talonMaster.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
-    	
+	   //_talonMaster.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+	   _talonMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10); //second parameter is timeout value in ms. 10 is default value
 	   	THRESH = 200;
 	   	
     	reset();
@@ -98,11 +112,16 @@ public class Shooter extends Subsystem {
     
     public void startMotors(){
     	if (pidEnabled) {
-    		_talonMaster.changeControlMode(TalonControlMode.Speed);
-    		System.out.println("mode: Speed");
+    		_talonMaster.config_kP(0, kP, 10);
+    		_talonMaster.config_kI(0, kD, 10);
+    		_talonMaster.config_kD(0, kD, 10);
+    		_talonMaster.config_kF(0, feed_forward, 10);
+    		_talonMaster.config_IntegralZone(0, Izone, 10);
+    		_talonMaster.set(ControlMode.Velocity, MOTOR_SPEED);
+    		System.out.println("mode: Velocity");
     	} else {
-    		_talonMaster.changeControlMode(TalonControlMode.Voltage);
-    		System.out.println("mode: Voltage");
+    		_talonMaster.set(ControlMode.PercentOutput, MOTOR_SPEED);
+    		System.out.println("mode: PercentOutput");
     	}
     	
     	
@@ -115,11 +134,11 @@ public class Shooter extends Subsystem {
       //kD = SmartDashboard.getNumber("kD", kD);
       //MOTOR_SPEED = SmartDashboard.getNumber("Velocity", MOTOR_SPEED);
     
-      _talonMaster.setPID(kP,  kI, kD, feed_forward, Izone, rampRate, channel); //in percentVBus this is ignored
-      _talonMaster.set(MOTOR_SPEED);
-	    SmartDashboard.putNumber("Position", _talonMaster.getEncPosition());
-	    SmartDashboard.putNumber("Velocity",  _talonMaster.getEncVelocity());
-	    SmartDashboard.putNumber("Velocity Raw",  _talonMaster.getEncVelocity()); //not to be used for graph
+      //_talonMaster.setPID(kP,  kI, kD, feed_forward, Izone, rampRate, channel); //in percentVBus this is ignored
+      //_talonMaster.set(MOTOR_SPEED);
+	    SmartDashboard.putNumber("Position", _talonMaster.getSensorCollection().getQuadraturePosition());
+	    SmartDashboard.putNumber("Velocity",  _talonMaster.getSensorCollection().getQuadratureVelocity());
+	    SmartDashboard.putNumber("Velocity Raw",  _talonMaster.getSensorCollection().getQuadratureVelocity()); //not to be used for graph
 	   	
 	  compressorEnabled = true;
     }
@@ -138,8 +157,7 @@ public class Shooter extends Subsystem {
     * 
     */
     public void stop(){
-    	_talonMaster.changeControlMode(TalonControlMode.PercentVbus);
-    	_talonMaster.set(0);
+    	_talonMaster.set(ControlMode.PercentOutput, 0);
     	compressorEnabled = false;
     }
     
@@ -148,7 +166,7 @@ public class Shooter extends Subsystem {
     }
     
     public double getVelocity(){
-    	return _talonMaster.getEncVelocity();
+    	return _talonMaster.getSensorCollection().getQuadratureVelocity();
     }
     
     public boolean isEnabled(){
@@ -165,13 +183,13 @@ public class Shooter extends Subsystem {
     
     public void shootOverrideSwitchState() {
     	if (pidEnabled) {
-    		_talonMaster.changeControlMode(TalonControlMode.Voltage);
-    		MOTOR_SPEED = VOLTAGE_MOTOR_SPEED;
+    		_talonMaster.set(ControlMode.PercentOutput, MOTOR_SPEED);
+    		//MOTOR_SPEED = VOLTAGE_MOTOR_SPEED;
     		pidEnabled = false;
     		System.out.println("switch to Voltage");
     	} else {
-    		_talonMaster.changeControlMode(TalonControlMode.Speed);
     		MOTOR_SPEED = PID_MOTOR_SPEED;
+    		_talonMaster.set(ControlMode.Velocity, MOTOR_SPEED);
     		pidEnabled = true;
     		System.out.println("switch to Speed");
     	}
@@ -179,7 +197,7 @@ public class Shooter extends Subsystem {
     }
 
     public void printReadyToShoot(){
-    	if (_talonMaster.getEncVelocity() < MOTOR_SPEED + THRESH || _talonMaster.getEncVelocity() > MOTOR_SPEED - THRESH){
+    	if (_talonMaster.getSensorCollection().getQuadratureVelocity() < MOTOR_SPEED + THRESH || _talonMaster.getSensorCollection().getQuadratureVelocity() > MOTOR_SPEED - THRESH){
     		SmartDashboard.putBoolean("Ready to Shoot", true);        	
     	} else {
     		SmartDashboard.putBoolean("Ready to Shoot", false);
