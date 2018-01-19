240c9a98a24d25ef8abb90f72d06217c6f079b00
jxfireruby <jxfireruby@gmail.com>
2018-01-19 17:45:41 -0500

    Shooter works
    firmware updated on talons

--- src/org/usfirst/frc/team5401/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/Shooter.java
@@ -20,7 +20,7 @@ public class Shooter extends Subsystem {
 	TalonSRX _talonSlave;	
 	
 	
-	private double VOLTAGE_MOTOR_SPEED = -7.8 / 10;
+	private double VOLTAGE_MOTOR_SPEED = .78;
 	private double PID_MOTOR_SPEED = -19900; //-19875 //-23750 original
 	private double MOTOR_SPEED = PID_MOTOR_SPEED;
 	private double feed_forward;
